8e5c12ab8d479fc2ffe46d81e583a063b6a2b63c
hrish239 <hrishikapatel29@gmail.com>
2022-09-20 12:23:32 +0530

    second commmit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let inputBtn = document.getElementById("input-btn")
-inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from addEventListener!")
-})
+let myLeads= []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function()
+{
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    
+}) 
+
+for (i=0; i<myLeads.length;i++){
+   ulEl.innerHTML += <li> + myLeads[i]+</li>
+}
